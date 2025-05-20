47481bea647722a03bcf3c727045d3148f14cc3f
Hatim Malak <hatim05042006@gmail.com>
2025-05-20 19:59:16 +0530

    Fix: Correct casing for useAuthStore import

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,7 +9,12 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import {server,app} from "./lib/socket.js"
 
-import path from "path"
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config()
 

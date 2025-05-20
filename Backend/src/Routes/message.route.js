4f63f033d55f0cf562914a7aa097eea0857cc77a
Hatim Malak <hatim05042006@gmail.com>
2025-05-20 20:45:03 +0530

    Fix: Correct casing for useAuthStore import

--- Backend/src/Routes/message.route.js
+++ Backend/src/Routes/message.route.js
@@ -5,7 +5,7 @@ import { getUsersForSidebar,getMessages,sendMessages } from "../controllers/mess
 const router = express.Router()
 
 router.get("/users",protectRoute,getUsersForSidebar)
-router.get("/:id",protectRoute,getMessages)
+router.get("/chat/:id",protectRoute,getMessages)
 router.post("/send/:id",protectRoute,sendMessages)
 
 export default router
